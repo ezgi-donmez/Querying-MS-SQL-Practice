3e5cd859d412c97a11c3a0e556c335f8fa0ec606
Ezgi <168410873+ezgi-donmez@users.noreply.github.com>
2026-07-25 14:43:56 +0300

    Add SQL queries for customer and sales analysis

--- practices.sql
+++ practices.sql
@@ -3,3 +3,18 @@
 -- Kaç farklı müşterinin alışveriş yaptığını gösterecek sorguyu yazınız.
 select count (master_id) 
   From flo;
+
+-- Toplam yapılan alışveriş sayısı ve ciroyu getirecek sorguyu yazınız.
+select 
+	sum(order_num_total_ever_online + order_num_total_ever_online) as toplam_alisveris, 
+	sum(customer_value_total_ever_online + customer_value_total_ever_offline) as toplam_ciro
+From flo;
+
+-- Alışveriş başına ortalama ciroyu getirecek sorguyu yazınız.
+select 
+	master_id,
+	(customer_value_total_ever_online + customer_value_total_ever_offline) / 
+	(order_num_total_ever_online + order_num_total_ever_online) as avg_ciro
+
+From flo;
+
