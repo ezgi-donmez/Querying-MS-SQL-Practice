232d3719493b400fb3d2a5febb71782d02391afa
Ezgi <168410873+ezgi-donmez@users.noreply.github.com>
2026-07-25 14:24:47 +0300

    Add comments to practices.sql for clarity
    Added comments to clarify the purpose of the SQL queries.

---
+++ practices.sql
@@ -0,0 +1,5 @@
+-- Customers isimli bir veritabanı ve verilen veri setindeki değişkenleri içerecek FLO isimli bir tablo oluşturunuz.
+
+-- Kaç farklı müşterinin alışveriş yaptığını gösterecek sorguyu yazınız.
+select count (master_id) 
+  From flo;
